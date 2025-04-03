522e3525f60458f4a3f74646abb4d7d90c8e19d7
VinceCampos <152839517+VinceCampos@users.noreply.github.com>
2025-04-03 03:28:50 +0000

    Lab Assignment 6 SOLID with Design Pattern

--- TestProgram.java
+++ TestProgram.java
@@ -1,16 +1,20 @@
 public class TestProgram {
     public static void main(String[] args) {
         
-        Student student1 = new Student("1234", "Orton", "Randy", "A Game Of Thrones");
+        Student student1 = new Student("43216", "Cena", "John", "The Time is Now");
         BookHandling process1 = new BookHandling(student1, new BookBorrower());
         process1.processBook();
 
-        Student student2 = new Student("2258", "Austin", "Steve", "Soul Therapy");
+        Student student2 = new Student("20195", "Rhodes", "Cody", "The Sport Journal");
         JournalHandling process2 = new JournalHandling(student2, new JournalBorrower());
         process2.processJournal();
 
-        Student student3 = new Student("0071", "Barnes", "Scottie", "The Hobbit");
+        Student student3 = new Student("55678", "Doncic", "Austin", "Harry Potter and the Chamber of Secrets");
         BookHandling process3 = new BookHandling(student3, new BookBorrower());
         process3.processBook();
+
+        Student student4 = new Student("94567", "Kent", "Clark", "Abiotic Stress and Legumes");
+        JournalHandling process4 = new JournalHandling(student4, new JournalBorrower());
+        process4.processJournal();
     }
 }
